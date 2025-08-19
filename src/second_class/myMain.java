da053df2efc372d14244aa1fe31ebf14ebb7ad24
Prathvi Raj Chauhan <prathvirajchauhandelhi6229@gmail.com>
2025-08-19 12:25:56 +0530

    third class done

--- src/second_class/myMain.java
+++ src/second_class/myMain.java
@@ -4,12 +4,12 @@ public class myMain {
     public static void main(String[] args) {
         Student one = new Student();
         one.display();
-        one.name = "ABHI";
+        one.name = "CHAMPAK";
         one.roll_no = 9;
 
-        Student two = new Student("PRASHANT ");
+        Student two = new Student("BABITA JI ");
         two.roll_no = 40;
-        Student three = new Student("TIWARI ", 69);
+        Student three = new Student("JETHA LAL", 69);
         Student four = new Student(three);
         one.display();
         two.display();
