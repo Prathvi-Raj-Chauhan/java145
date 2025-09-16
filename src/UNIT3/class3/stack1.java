e56836ab972e94902b88a2601e34162549ef8bd7
Prathvi Raj Chauhan <prathvirajchauhandelhi6229@gmail.com>
2025-09-16 09:23:05 +0530

    stack and vector

--- src/UNIT3/class3/stack1.java
+++ src/UNIT3/class3/stack1.java
@@ -1,4 +1,17 @@
 package UNIT3.class3;
 
+import java.util.Stack;
+
 public class stack1 {
+    public static void main(String[] args) {
+        Stack<String> stk = new Stack<>();
+        stk.push("First");
+        stk.push("Second");
+        System.out.println(stk.pop());
+        System.out.println(stk.peek());
+        System.out.println(stk.size());
+
+        stk.clear();
+        System.out.println(stk.isEmpty());
+    }
 }
